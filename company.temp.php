dc476b6914f05a565ecf0528b8ab7c88f63de325
Cristoffer Arn <cristoffer.arn@campushultsfred.se>
2013-03-12 12:01:11 +0100

    Web-friendly background

---
+++ company.temp.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Template Name: Companies & Workplaces
+*/
+?>
+
+<?php get_header(); ?>
+<div id="main">
+	<div id="content">
+		<h1>Main Area</h1>
+		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+			<p><?php the_content(); ?></p>
+			<hr>
+		<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.') ?></p>
+		<?php endif; ?>
+	</div>
+</div>
+<?php get_footer(); ?>
